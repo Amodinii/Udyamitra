7d64074f2ecd2d5ab98de1bf789d51b76139e4b8
Amodinii <apamodini@gmail.com>
2025-07-28 13:12:41 +0530

    Human In the loop

---
+++ Servers/EligibilityChecker/EligibilityFlow/state.py
@@ -0,0 +1,24 @@
+from typing import Optional, List, Dict, Union
+from pydantic import BaseModel
+from utility.model import EligibilityCheckRequest, UserProfile
+
+class EligibilityState(BaseModel):
+    user_profile: Dict[str, Union[str, int, float]] = {}
+    scheme_name: str
+    context_entities: Optional[Dict[str, Union[str, List[str]]]] = None
+    query: Optional[str] = None
+    retrieved_documents: Optional[str] = None
+
+    eligibility_response: Optional[Dict] = None
+    follow_up_questions: Optional[List[str]] = []
+    missing_fields: Optional[List[str]] = []
+    final_explanation: Optional[str] = None
+    last_answer_field: Optional[str] = None
+
+    def to_request(self) -> EligibilityCheckRequest:
+        return EligibilityCheckRequest(
+            scheme_name=self.scheme_name,
+            user_profile=UserProfile(**self.user_profile),
+            context_entities=self.context_entities,
+            query=self.query
+        )
